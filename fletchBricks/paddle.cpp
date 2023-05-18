f55cee075ca0aefe7070d0a8d1bb82906cbc52fd
fletchgraham <sfletchgraham@gmail.com>
2023-05-18 08:09:02 -0600

    move paddle clamp to private

--- fletchBricks/paddle.cpp
+++ fletchBricks/paddle.cpp
@@ -23,6 +23,24 @@ public:
 
     }
 
+    bool isBallCollide(Ball ball)
+    {
+        float diff = pos.x - ball.pos.x;
+
+        // Take absolute value of difference
+        diff = (diff > 0.0f) ? diff : -diff;
+
+        return (
+            // Our x-difference is small enough
+            diff <= width / 2.0f &&
+            // We are in the correct y-position
+            ball.pos.y <= pos.y + 5.0f && ball.pos.y >= pos.y - 5.0f &&
+            // The ball is moving down
+            ball.vel.y > 0.0f
+        );
+    }
+
+private:
     void ClampX()
     {
         float leftExtremePos = leftLimit + width/2;
@@ -36,23 +54,5 @@ public:
         {
             pos.x = rightExtremePos;
         }
-    }
-
-    bool isBallCollide(Ball ball)
-    {
-
-	float diff = pos.x - ball.pos.x;
-
-	// Take absolute value of difference
-	diff = (diff > 0.0f) ? diff : -diff;
-
-	return (
-		// Our x-difference is small enough
-		diff <= width / 2.0f &&
-		// We are in the correct y-position
-		ball.pos.y <= pos.y + 5.0f && ball.pos.y >= pos.y - 5.0f &&
-		// The ball is moving down
-		ball.vel.y > 0.0f
-        );
     }
 };
